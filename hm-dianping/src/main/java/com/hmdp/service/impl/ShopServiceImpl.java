4b9a6aac1a44a94d445a3bec5b0afd9ca0c15a00
coffee-code1 <qwertLU20061122@git>
2026-07-20 15:52:06 +0800

    依据Redis中的Geo更新了附近商铺查询功能

--- hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,22 +1,35 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.hmdp.utils.CacheClient;
+import com.hmdp.utils.SystemConstants;
+import org.springframework.data.geo.Circle;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.GeoResult;
+import org.springframework.data.geo.GeoResults;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
 import static com.hmdp.utils.RedisConstants.LOCK_SHOP_KEY;
+import static com.hmdp.utils.RedisConstants.SHOP_GEO_KEY;
 
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
@@ -34,7 +47,7 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         }
         return Result.ok(shop);
     }
-    //逻辑过期的方法
+
     public Shop queryWithMutex(Long id) {
         return cacheClient.queryWithLogicExpire(
                 CACHE_SHOP_KEY,
@@ -45,6 +58,69 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
                 CACHE_SHOP_TTL,
                 TimeUnit.MINUTES
         );
+    }
+
+    @Override
+    public Result queryShopByType(Integer typeId, Integer current, Double x, Double y) {
+        //没有坐标，正常分页查询
+        if (x == null || y == null) {
+            Page<Shop> page = query()
+                    .eq("type_id", typeId)
+                    .page(new Page<>(current, SystemConstants.DEFAULT_PAGE_SIZE));
+            return Result.ok(page.getRecords());
+        }
+
+        //计算分页查询的起始跟结束
+        int pageSize = SystemConstants.DEFAULT_PAGE_SIZE;
+        int from = (current - 1) * pageSize;
+        int end = current * pageSize;
+
+        String key = SHOP_GEO_KEY + typeId;
+        //根据坐标按照圆形范围查询,这里是旧写法
+        GeoResults<RedisGeoCommands.GeoLocation<String>> results = stringRedisTemplate.opsForGeo().radius(
+                key,
+                new Circle(new Point(x, y), new Distance(5000)),
+                RedisGeoCommands.GeoRadiusCommandArgs.newGeoRadiusArgs()
+                        .includeDistance()
+                        .sortAscending()
+                        .limit(end)
+        );
+
+        //查不到返回空
+        if (results == null) {
+            return Result.ok(Collections.emptyList());
+        }
+
+        //获取点位封装对象 GeoLocation
+        List<GeoResult<RedisGeoCommands.GeoLocation<String>>> content = results.getContent();
+        if (content.size() <= from) {
+            return Result.ok(Collections.emptyList());
+        }
+
+        List<Long> ids = new ArrayList<>(content.size() - from);
+        Map<String, Distance> distanceMap = content.stream()
+                .skip(from)
+                .peek(result -> ids.add(Long.valueOf(result.getContent().getName())))//收集商铺id
+                .collect(Collectors.toMap(
+                        result -> result.getContent().getName(),
+                        GeoResult::getDistance
+                ));
+
+        String idStr = ids.stream()
+                .map(String::valueOf)//转成字符串
+                .collect(Collectors.joining(","));//也可以用strutils工具包
+        List<Shop> shops = query()
+                .in("id", ids)
+                .last("ORDER BY FIELD(id," + idStr + ")")
+                .list();//这样的方法查询也可以解决数据库查询结果顺序跟传入id不一致的问题
+        shops.forEach(shop -> {
+            Distance distance = distanceMap.get(shop.getId().toString());
+            if (distance != null) {
+                shop.setDistance(distance.getValue());
+            }
+        });
+
+        return Result.ok(shops);
     }
 
     @Override
